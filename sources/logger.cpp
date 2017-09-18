a1dd9aaf3aa753eb07beec9a3be8e3d8c993382b
Sergey Savin <savin@smartek.ru>
2017-09-18 16:16:45 +0300

    wchar_t add to logger()

--- sources/logger.cpp
+++ sources/logger.cpp
@@ -45,6 +45,26 @@ std::ostream& operator<<(std::ostream& os, const logger& )
     return os;
 }
 
+std::wostream& operator<<(std::wostream& os, const logger& )
+{
+    using namespace std::chrono;
+    high_resolution_clock::time_point p = high_resolution_clock::now();
+    milliseconds ms = duration_cast<milliseconds>(p.time_since_epoch());
+
+    seconds s = duration_cast<seconds>(ms);
+    std::time_t in_time_t = s.count();
+    std::size_t fractional_seconds = ms.count() % 1000;
+
+
+    //auto now = std::chrono::system_clock::now();
+    //auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    /*std::wbuffer_convert<std::codecvt_utf8<wchar_t>> conv(std::cout.rdbuf());
+    std::wostream out(&conv);*/
+    os << std::put_time(std::localtime(&in_time_t), L"%X")<< L"." << std::setw(3) << std::setfill(L'0') << fractional_seconds << L" [" << std::this_thread::get_id() << L"] ";
+    return os;
+}
+
 
 namespace Utils
 {
